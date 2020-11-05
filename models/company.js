f9922e2274a032f29efb3b003bddd171f0aad6d0
unknown <jreidmke@gmail.com>
2020-11-05 14:13:09 -0600

    init company model

---
+++ models/company.js
@@ -0,0 +1,37 @@
+//import db maybe some other stuff
+
+class Company {
+
+
+    //async static search{
+
+    //makes a db.query and returns a "filtered list of handles and names"
+
+    //OOOOOOOH WEE, we're gonna have to do some of that weird SQL stuff with words and stuff.
+
+    // So it'll be something like
+
+    //SELECT handle, name FROM companies WHERE name LIKE "%{name}%";}
+
+    /*
+        async static min_employees {
+            SELECT c.handles, j.titles
+            FROM companies AS c
+            JOIN jobs AS j
+            ON c.handle=j.company_handle
+            WHERE c.min_employees > {WHATEVER IS PASSED IN QUERY STRING}
+        }
+    */
+
+    /*
+        async static max_employees {
+            SELECT c.handles, j.titles
+            FROM companies AS c
+            JOIN jobs AS j
+            ON c.handle=j.company_handle
+            WHERE c.min_employees < {WHATEVER IS PASSED IN QUERY STRING}
+        }
+    */
+
+    
+}
