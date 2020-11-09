d5a899c3826eacec8ac77156a1c09cd94a274bf1
unknown <jreidmke@gmail.com>
2020-11-09 15:13:19 -0600

    new routing for job positing

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,5 +7,32 @@ const Job = require('../models/Job');
 
 const router = express.Router({ mergeParams: true });
 
-module.exports = router;
+router.get('/', async(req, res, next) => {
+    try {
+        if('search' in req.query) {
+            console.log(req.query);
+            return res.json(await Job.search(req.query.search));
+        }
+        else if('min_salary' in req.query) {
+            return res.json(await Job.min_salary(req.query.min_salary));
+        }
+        else if('min_equity' in req.query) {
+            return res.json(await Job.min_equity(req.query.min_equity));
+        }
+        else {
+            return res.json({message: "Please search by title, salary or equity."});
+        }
+    } catch (error) {
+        return next(error)
+    }
+})
 
+router.post('/', async(req, res, next) => {
+    try {
+        return res.json(await Job.createJob(req.body));
+    } catch (error) {
+        return next(error);
+    }
+})
+
+module.exports = router;
