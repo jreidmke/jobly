865af3e37ddb1c6d1bcb57c020845c2115624c8f
unknown <jreidmke@gmail.com>
2020-11-10 11:41:01 -0600

    new validation for post/patch companies

--- routes/companies.js
+++ routes/companies.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const ExpressError = require('../helpers/expressError');
 const Company = require('../models/company');
 const { validate } = require('jsonschema');
-// const {  } = require('../schema');
-const { newCompany, updateCompany } = require('../schema');
+const newCompany = require('../schema/newCompany.json');
+const updateCompany = require('../schema/updateCompany.json');
 
 
 router.get('/', async(req, res, next) => {
@@ -42,28 +42,28 @@ router.post('/', async(req, res, next) => {
 })
 
 router.get('/:handle', async(req, res, next) => {
-    //return one company via handle
-    //Company.get()
-    const resp = await Company.get(req.params.handle);
-    return res.json({company: resp});
-
-    //return json of {company: {handle:, name:, etc...}}
+    try {
+        const resp = await Company.get(req.params.handle);
+        return res.json({company: resp});
+    } catch (error) {
+        return next(error);
+    }
 })
 
-//IN THIS ROUTE, MAKE SURE TO INCLUDE JSON VALIDATION FROM JSONSCH3MA
 router.patch('/:handle', async(req, res, next) => {
     try {
         if ('handle' in req.body) {
           throw new ExpressError('You are not allowed to change the handle.', 400);
         }
-
+        const validation = validate(req.body, updateCompany);
+        if(!validation.valid) {
+            throw new ExpressError(validation.errors.map(error => error.stack), 400);
+        }
         const company = await Company.update(req.params.handle, req.body);
         return res.json({ company });
       } catch (err) {
         return next(err);
       }
-    //company.update
-    //return json of {company: {handle:, name:, etc...}}
 })
 
 router.delete('/:handle', async(req, res, next) => {
