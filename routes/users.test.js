ba123738c46bb41e604b245ff9cef34f92c732d7
unknown <jreidmke@gmail.com>
2020-11-13 11:42:31 -0600

    updated testing

--- routes/users.test.js
+++ routes/users.test.js
@@ -31,3 +31,45 @@ afterEach(async() => {
 afterAll(async() => {
     await db.end();
 })
+
+describe("GET /users", () => {
+    test("should return list of all users", async() => {
+        const resp = await request(app).get(`/users`);
+        console.log(user);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body[0].username).toBe(user.username);
+        expect(resp.body[0].email).toBe(user.email);
+    })
+})
+
+describe("GET /users/:id", () => {
+    test("should return single user", async () => {
+        const resp = await request(app).get(`/users/${user.username}`);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.username).toBe(user.username);
+        expect(resp.body.email).toBe(user.email);
+    })
+
+    test("should return error msg with invalid username", async() => {
+        const resp = await request(app).get(`/users/pizza`);
+        expect(resp.statusCode).toBe(404);
+        expect(resp.body["message"]).toEqual("Invalid username");
+
+    })
+})
+
+describe("POST /users", () => {
+    test("should add one user to db", async() => {
+        const data = {
+            "username": "ted",
+            "password": "pizza",
+            "first_name": "James",
+            "last_name": "Reid",
+            "email": "ted@gmail.com",
+            "photo_url": "google.com",
+            "is_admin": true
+        };
+        const resp = await request(app).post(`/users`).send(data);
+        expect(resp.statusCode).toBe(201);
+    })
+})
